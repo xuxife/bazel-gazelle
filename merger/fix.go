edfa98475db10bb35227f3b43f97cf3f92a7eb95
Chase Starr <chasestarr@gmail.com>
2025-01-16 01:14:39 -0800

    merger: handle ident load (#2014)
    **What type of PR is this?**
    Bug fix
    
    **What package or component does this PR mostly affect?**
    merger
    
    **What does this PR do? Why is it needed?**
    I found that used idents in assignment expression rhs position have
    their load statements removed. This PR adds those idents to used symbols
    collection in `FixLoads` fn.

--- merger/fix.go
+++ merger/fix.go
@@ -88,6 +88,12 @@ func FixLoads(f *rule.File, knownLoads []rule.LoadInfo) {
 			} else {
 				return
 			}
+		} else if ae, ok := x.(*bzl.AssignExpr); ok {
+			if id, ok := ae.RHS.(*bzl.Ident); ok {
+				idents = append(idents, id)
+			} else {
+				return
+			}
 		} else {
 			return
 		}
