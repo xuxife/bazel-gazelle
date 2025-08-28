a16c3b0981d9916812c0c322cdcf165a5e92c1d5
xuxife <xuxife@gmail.com>
2025-08-28 11:14:34 +1000

    fix copyTree

--- cmd/fetch_repo/copy_tree.go
+++ cmd/fetch_repo/copy_tree.go
@@ -34,26 +34,26 @@ func copyTree(destRoot, srcRoot string) error {
 			return nil
 		}
 		dest := filepath.Join(destRoot, rel)
-
-		if info.IsDir() {
-			return os.Mkdir(dest, 0o777)
-		}
-
-		r, err := os.Open(src)
-		if err != nil {
-			return err
-		}
-		defer r.Close()
-		w, err := os.Create(dest)
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if cerr := w.Close(); err == nil && cerr != nil {
-				err = cerr
+		switch {
+		case info.Mode().IsDir():
+			err = os.Mkdir(dest, 0o777)
+		case info.Mode().IsRegular():
+			r, err := os.Open(src)
+			if err != nil {
+				return err
 			}
-		}()
-		_, err = io.Copy(w, r)
+			defer r.Close()
+			w, err := os.Create(dest)
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if cerr := w.Close(); err == nil && cerr != nil {
+					err = cerr
+				}
+			}()
+			_, err = io.Copy(w, r)
+		}
 		return err
 	})
 }
